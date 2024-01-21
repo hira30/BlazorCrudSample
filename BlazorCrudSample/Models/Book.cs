ce12a859a4f2ba1b014508e842524b9eaa5c8786
hirahira <th11282@gmail.com>
2024-01-21 16:38:07 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorCrudSample/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorCrudSample.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
+        public int PublihedYear { get; set; }
+    }
+}
